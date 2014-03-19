25680bc84a9cc37246b0c7bbef4fe725229cf3bd
travisb22 <travis.britt@ymail.com>
2014-03-18 21:32:51 -0400

    lab7

--- Britt_Travis_Lab7.js
+++ Britt_Travis_Lab7.js
@@ -17,10 +17,14 @@ for (var videoGame in jsonData.myGame) {
 		
 	}else if (videoGame === 1) {
 		
-		var mySecondVideoGame = new myVideoGame(jsondata.myVideoGame[Game].year, jsonData.myVideoGame[Game].name, jsonData.myVideoGame[Game].creator, jsoData.myVidoGame[Game].studio);
+		var myLeastFavoriteVideoGame = new myVideoGame(jsondata.myVideoGame[Game].year, jsonData.myVideoGame[Game].name, jsonData.myVideoGame[Game].creator, jsoData.myVidoGame[Game].studio);
 	};
 };
-// outputs for coad
+// outputs for code
 myVideoGame();
+
+console.log("The game I love to play the most in my spare time is" + myFavoriteVideoGame.title + ". It was made by" + myFavoriteVideoGame.developer + " in the year of" + myFavoriteVideoGame.year + ". The studio that was responsible for helping create it was " + myVideoGame.studio);
+
+console.log("The game I realy dont like to play is" + myLeastFavoriteVideoGame.title + ". It was made by" + myLeastFavoriteVideGame.developer + ", in the year of" + myLeastFavoriteVideoGame.year + ". The studio that was responsible for helping create it was " + myLeastFavoriteVideoGame.studio); 
 
 
