d33414778bf356b6b659c7d75060172d0cf2b377
travisb22 <travis.britt@ymail.com>
2014-03-18 22:08:49 -0400

    lab7_3

---
+++ Britt_Travis_Lab7 json.js
@@ -0,0 +1,22 @@
+//alert("JavaScript works!");
+
+// My variable
+var jsonData = {
+	
+	"myVideoGame":
+	[
+		{
+			"title": "League of Legends",
+			"year": 2008,
+			"studio": "Riot Games",
+			"CEO": "Brandon Beck"
+		},
+		{
+			
+			"title": "Dirt",
+			"year": 2011,
+			"studio": "Codemasters",
+			"CEO": "Rod Cousens"
+		}
+	]
+};
--- Britt_Travis_Lab7.js
+++ Britt_Travis_Lab7.js
@@ -2,7 +2,7 @@
 // variable
 var myVideoGame = function(creator, studio, name, year){
 // conection to json file	
-	this.creator = creator;
+	this.CEO = CEO;
 	this.studio = studio;
 	this.year = year;
 	this.name = name;
@@ -13,18 +13,18 @@ for (var videoGame in jsonData.myGame) {
 // else statements
 	if (videoGame === 0){
 		
-		var myFavoriteVideoGame = new myVideoGame(jsondata.myVideoGame[Game].year, jsonData.myVideoGame[Game].name, jsonData.myVideoGame[Game].creator, jsoData.myVidoGame[Game].studio);
+		var myFavoriteVideoGame = new myVideoGame(jsondata.myVideoGame[Game].year, jsonData.myVideoGame[Game].name, jsonData.myVideoGame[Game].CEO, jsoData.myVidoGame[Game].studio);
 		
 	}else if (videoGame === 1) {
 		
-		var myLeastFavoriteVideoGame = new myVideoGame(jsondata.myVideoGame[Game].year, jsonData.myVideoGame[Game].name, jsonData.myVideoGame[Game].creator, jsoData.myVidoGame[Game].studio);
+		var myLeastFavoriteVideoGame = new myVideoGame(jsondata.myVideoGame[Game].year, jsonData.myVideoGame[Game].name, jsonData.myVideoGame[Game].CEO, jsoData.myVidoGame[Game].studio);
 	};
 };
 // outputs for code
 myVideoGame();
 
-console.log("The game I love to play the most in my spare time is" + myFavoriteVideoGame.title + ". It was made by" + myFavoriteVideoGame.developer + " in the year of" + myFavoriteVideoGame.year + ". The studio that was responsible for helping create it was " + myVideoGame.studio);
+console.log("The game I love to play the most in my spare time is" + myFavoriteVideoGame.title + ". It was made by" + myFavoriteVideoGame.CEO + " in the year of" + myFavoriteVideoGame.year + ". The studio that was responsible for helping create it was " + myFavoriteVideoGame.studio);
 
-console.log("The game I realy dont like to play is" + myLeastFavoriteVideoGame.title + ". It was made by" + myLeastFavoriteVideGame.developer + ", in the year of" + myLeastFavoriteVideoGame.year + ". The studio that was responsible for helping create it was " + myLeastFavoriteVideoGame.studio); 
+console.log("The game I realy dont like to play is" + myLeastFavoriteVideoGame.title + ". It was made by" + myLeastFavoriteVideGame.CEO + ", in the year of" + myLeastFavoriteVideoGame.year + ". The studio that was responsible for helping create it was " + myLeastFavoriteVideoGame.studio); 
 
 
